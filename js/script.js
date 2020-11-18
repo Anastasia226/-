3f2295925a4ff242614a4f5cbecdeb735fcd5cc0
Anastasia <bagaeva00@yandex.ru>
2020-11-18 19:21:21 +0300

    fix adaptiv

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let menuToggle = document.querySelector(".menu-toggle");
 let menu = document.querySelector("nav .items");
 let arrow = document.querySelector(".news-arrow");
+let widthRewiew_1 = $(".rewiew-1").innerWidth(); //ширина блока отзывов 1
+let widthRewiew_2 = $(".rewiew-3").innerWidth(); //ширина блока отзывов 3
 //меню для мобильной версии
 menuToggle.addEventListener("click", function (event) {
   event.preventDefault();
@@ -8,6 +10,8 @@ menuToggle.addEventListener("click", function (event) {
 });
 
 $(document).ready(function () {
+  $(".rewiew-block-1").css("left", widthRewiew_1 - 185);
+  $(".rewiew-block-3").css("left", widthRewiew_2 - 185);
   //слайдер новостей
   arrow.classList.toggle("slick-next");
   $(".slider-news").slick({
@@ -33,7 +37,7 @@ $(document).ready(function () {
   //слайдер отзывов
   $(".slider-rewiew").slick({
     slidesToShow: 1,
-    centerPadding: "50px",
+    centerPadding: "-100px",
     centerMode: true,
     prevArrow: ` <svg class="icon icon-back slick-prev">
                 <use xlink:href="img/icons.svg#arrow"></use>
@@ -50,6 +54,26 @@ $(document).ready(function () {
       },
     ],
   });
+  //сладйер партнеров
+  $(".slider-partners").slick({
+    arrows: false,
+    slidesToShow: 5,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1180,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+    ],
+  });
   $(".rewiew-arrow-prev").append($(".slider-rewiew>.slick-prev"));
   $(".rewiew-arrow-next").append($(".slider-rewiew>.slick-next"));
   let currentSlide_r = $(".slider-rewiew").slick("slickCurrentSlide") + 1;
@@ -59,4 +83,11 @@ $(".slider-news").on("afterChange", function (event, slick, currentSlide) {
 });
 $(".slider-rewiew").on("afterChange", function (event, slick, currentSlide_r) {
   $(".page-active").text(currentSlide_r + 1);
+});
+$(window).resize(function (e) {
+  $(".rewiew-arrow-prev").append($(".slider-rewiew>.slick-prev"));
+  $(".rewiew-arrow-next").append($(".slider-rewiew>.slick-next"));
+  $(".news-arrow").append($(".slider-news>.slick-next"));
+  $(".rewiew-block-1").css("left", widthRewiew_1 - 185);
+  $(".rewiew-block-3").css("left", widthRewiew_2 - 185);
 });
